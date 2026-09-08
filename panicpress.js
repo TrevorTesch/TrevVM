2afd002b9b90b7ac3b139318a1d71029fc1ed3f8
Trevor Tesch <tdltesch@gmail.com>
2026-09-08 16:27:33 -0400

    well see if wokrs

---
+++ panicpress.js
@@ -0,0 +1,18 @@
+
+(function() {
+    const safeSite = 'https://wcpss.instructure.com';
+    const panicKeyCode = 'Equal';
+
+    function redirectToSafeSite() {
+        window.location.replace(safeSite);
+    }
+
+    window.addEventListener('keydown', (event) => {
+        if (event.code === panicKeyCode && !event.altKey && !event.ctrlKey && !event.shiftKey && !event.metaKey) {
+            event.preventDefault();
+            redirectToSafeSite();
+        }
+    });
+
+    console.log('Panic key active: Press the equal key to hide this page.');
+})();
